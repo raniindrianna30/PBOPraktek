ba4b2572a8d6237b0e0012c45d076b9514cfa126
Rani Indrianna <89647589+raniindrianna30@users.noreply.github.com>
2021-12-13 11:10:51 +0700

    push w12 javaCollectionList

---
+++ W12_PBO_PR/src/JavaCollectionList/ServicePackage.java
@@ -0,0 +1,24 @@
+package JavaCollectionList;
+
+public class ServicePackage {
+    private String service;
+    private int value;
+	
+	public String getService() {
+            return service;
+	}
+	
+	public void setService(String service) {
+            this.service = service;
+	}
+	
+	public int getValue() {
+             return value;
+	}
+	
+	public void setValue(int value) {
+            this.value = value;
+	}
+}
+
+
